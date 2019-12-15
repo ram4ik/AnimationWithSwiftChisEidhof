685bcb2893e49848226102085c1b83df751945d4
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-15 22:10:46 +0200

    init

--- AnimationWithSwiftChisEidhof/ContentView.swift
+++ AnimationWithSwiftChisEidhof/ContentView.swift
@@ -8,9 +8,30 @@
 
 import SwiftUI
 
+struct Dump: View {
+    var result = ""
+    init<A>(_ value: A) {
+        dump(value, to: &result)
+    }
+    var body: Text { Text(result) }
+}
+
 struct ContentView: View {
+    @State var selected = false
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Rectangle()
+                .fill(selected ? Color.green : Color.pink)
+                .frame(width: selected ? 300 : 200, height: 100)
+                .offset(y: selected ? -300 : 0)
+                .animation(Animation.default.speed(0.1))
+            
+            Button(action: {
+                self.selected.toggle()
+            }, label: { selected ? Text("Deselected") : Text("Select") })
+            
+            Dump(Rectangle().fill(Color.pink).frame(width: 200, height: 100))
+        }
     }
 }
 
